98f18ff586e8b6e4c8e40980202ef8062261a178
unknown <kaiokot@gmail.com>
2021-04-30 17:06:09 -0300

    rename

--- curso02 - Machine Learning Classificacao por tras panos/dados.py
+++ curso02 - Machine Learning Classificacao por tras panos/dados.py
@@ -15,3 +15,20 @@ def carregar_acessos():
         Y.append(int(comprou))
         
     return X, Y
+
+
+def carregar_cursos():
+    X = []
+    Y = []
+    arquivo = open("cursos.csv", "rb")
+    leitor = csv.reader(arquivo)
+
+    leitor.next()
+
+    for home, como_funciona, contato, comprou in leitor:
+        dados = [int(home), int(como_funciona), int(contato)]
+        X.append(dados)
+
+        Y.append(int(comprou))
+        
+    return X, Y
