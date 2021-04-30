f2fd515237d319b6ae64c41cfdb9cd3d69bf011c
unknown <kaiokot@gmail.com>
2021-04-30 17:04:39 -0300

    .

---
+++ curso02 - Machine Learning Classificacao por tras panos/dados.py
@@ -0,0 +1,17 @@
+import csv
+
+def carregar_acessos():
+    X = []
+    Y = []
+    arquivo = open("acesso.csv", "rb")
+    leitor = csv.reader(arquivo)
+
+    leitor.next()
+
+    for home, como_funciona, contato, comprou in leitor:
+        dados = [int(home), int(como_funciona), int(contato)]
+        X.append(dados)
+
+        Y.append(int(comprou))
+        
+    return X, Y
